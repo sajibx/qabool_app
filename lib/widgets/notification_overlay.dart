2543cf4b86028f70c04acac6fe4ba7990b2466bc
sajib <ashiqur.rahman@technuf.com>
2026-03-15 17:53:26 +0600

    Restore missing imports in NotificationOverlay

--- lib/widgets/notification_overlay.dart
+++ lib/widgets/notification_overlay.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:qabool_app/theme.dart';
+import 'package:qabool_app/main.dart';
 import '../utils/image_utils.dart';
 import 'dart:async';
 
